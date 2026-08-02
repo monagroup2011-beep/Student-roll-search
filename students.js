0fd6cfe96d865dec4fae52448525975928296665
monagroup2011-beep <monagroup2011@gmail.com>
2026-08-03 00:07:17 +0530

    Update students.js

--- students.js
+++ students.js
@@ -1,1 +1,5 @@
-
+const students = [
+  { name: "Ritesh Patel", class: "10", section: "A", roll: "101" },
+  { name: "Rahul Gupta", class: "10", section: "B", roll: "102" },
+  { name: "Riya Sahu", class: "9", section: "A", roll: "201" }
+];
